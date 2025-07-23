61c9bb6f8bcedf3761fb8a09dea4d9ddcc32b252
FAYEZ-LOQ\fayez <fayezshahid167@gmail.com>
2025-07-23 23:30:49 +0200

    editted endpoints.ts

--- src/app/constants/endpoints.ts
+++ src/app/constants/endpoints.ts
@@ -7,4 +7,4 @@ export const ALBUM_IMAGES_API = `${API_BASE_URL}/album-images`;
 export const IMAGES_API = `${API_BASE_URL}/images`;
 export const AUTH_API = `${API_BASE_URL}/auth`;
 
-export const IMAGE_UPLOAD_URL = environment.imageUploadUrl;
+export const IMAGE_UPLOAD_URL = `${environment.imageUploadUrl}/`;
